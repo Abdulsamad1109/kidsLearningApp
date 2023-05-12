a3f3debfbe5acf2a9dcaeebdcf31205dd1acbd48
Abdulsamad1109 <gbadebosamad0@gmail.com>
2023-05-12 23:21:20 +0100

    finish some adjustment

--- main.js
+++ main.js
@@ -13,22 +13,22 @@ document.getElementById("container").style.display = "none";
 var i = 0
 var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var words = ["Apple","Ball","Cup","Dog","Elephant","Fish","Giraffe","Horse","Ink","Jug","Knife","Lime","Mango","Nail","Ostrich","Potato","Quail","Rabbit","Snail","Tomato","Umbrella","Vase","Watch","Xylophone","Yatch","Zebra"]
-var imageArray = ["Apple.jPg","ball.jpg","cup.jpg","dog.jpg","Elephant.jpg","Fish.jpg","Giraffe.jpg","Horse.jpg","Ink.jpg","Jug.jpg","Knife.jpg","Lime.jpg","Mango.jpg","Nail.jpg","Ostrich.jpg","potato.jpg","Quail.jpg","Rabbit.jpg","Snail.jpg","Tomato.jpg","Umbrella.jpg","Vase.jpg","Wristwatch.jpg","Xylophone.jpg","Yatch.jpg","Zebra.jpg"]
+var imageArray = ["apple.jPg","ball.jpg","cup.jpg","dog.jpg","Elephant.jpg","Fish.jpg","Giraffe.jpg","Horse.jpg","Ink.jpg","Jug.jpg","Knife.jpg","Lime.jpg","Mango.jpg","Nail.jpg","Ostrich.jpg","potato.jpg","Quail.jpg","Rabbit.jpg","Snail.jpg","Tomato.jpg","Umbrella.jpg","Vase.jpg","Wristwatch.jpg","Xylophone.jpg","Yatch.jpg","Zebra.jpg"]
 var myImage = document.getElementById("myImg");
 document.getElementById("display").innerText = (alphabet[i] + " for " + words[i]);
 function btnNext(){
-    i++;
+    i++
     document.getElementById("display").innerHTML = (alphabet[i] + " for " + words[i]);
     myImage.setAttribute("src", imageArray[i])
 
-    if(i == 25){
-        document.getElementById("Next-btn").remove()
-    }
     if(i == 1){
         document.getElementById("Previous-btn").style.display = "block"
     }
 
-  
+    if(i == 26){
+        i = 0
+    }
+
 }
 
 
@@ -39,7 +39,8 @@ function btnPrevious(){
     document.getElementById("display").innerHTML = (alphabet[i] + " for " + words[i]);
     myImage.setAttribute("src", imageArray[i]);
     document.getElementById("Next-btn").style.display = "block"
+
+    if(i < 0){
+        i = 0
+    }
 }
-
-
-
